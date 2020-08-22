753ef4ef83f7cb1d3292e98daf34136a9b93f432
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2020-08-22 23:01:21 +0530

    define job to determine whether a site is up

---
+++ app/Jobs/CheckSite.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Site;
+use Exception;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Http;
+
+class CheckSite implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $site;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(Site $site)
+    {
+        $this->site = $site;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        info('running Checksite', ['site' => $this->site]);
+
+        try {
+            $response = Http::get($this->site->url);
+
+            $this->site->online = $response->successful();
+
+        } catch (Exception $ex) {
+            info('checkSite EXCEPTION', ['ex' => $ex->getMessage()]);
+
+            $this->site->online = false;
+        }
+
+        $this->site->save();
+    }
+}
