3d480576dd8bb754d1517829634038417d8e063e
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2020-08-22 20:46:51 +0530

    define model, controller, migration, and seed for Site model

---
+++ database/seeds/SiteSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class SiteSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('sites')->insert([
+            [
+                'url' => 'http://www.example.com',
+                'online' => true,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'url' => 'http://www.example.org',
+                'online' => true,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
+    }
+}
