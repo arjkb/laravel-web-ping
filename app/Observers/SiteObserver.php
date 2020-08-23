2d237d244dfa2aeda1f8f8ce4f205045020359da
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2020-08-23 18:36:58 +0530

    scaffold an observer for Site

---
+++ app/Observers/SiteObserver.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Observers;
+
+use App\Site;
+
+class SiteObserver
+{
+    /**
+     * Handle the site "created" event.
+     *
+     * @param  \App\Site  $site
+     * @return void
+     */
+    public function created(Site $site)
+    {
+        //
+    }
+
+    /**
+     * Handle the site "updated" event.
+     *
+     * @param  \App\Site  $site
+     * @return void
+     */
+    public function updated(Site $site)
+    {
+        info('SiteObserver: updated()', ['site_id' => optional($site)->id]);
+
+        if ($site->online) {
+            // site came back on; add ended_at for downtime
+            optional($site->downtimes()
+                    ->whereNull('ended_at')
+                    ->latest('started_at')
+                    ->first()
+            )->update(['ended_at' => now()]);
+
+        } else {
+            // site went down; add entry in downtime
+            $site->downtimes()->create();
+
+        }
+    }
+
+    /**
+     * Handle the site "deleted" event.
+     *
+     * @param  \App\Site  $site
+     * @return void
+     */
+    public function deleted(Site $site)
+    {
+        //
+    }
+
+    /**
+     * Handle the site "restored" event.
+     *
+     * @param  \App\Site  $site
+     * @return void
+     */
+    public function restored(Site $site)
+    {
+        //
+    }
+
+    /**
+     * Handle the site "force deleted" event.
+     *
+     * @param  \App\Site  $site
+     * @return void
+     */
+    public function forceDeleted(Site $site)
+    {
+        //
+    }
+}
