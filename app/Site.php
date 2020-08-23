85558abc72b0aeafeee6f1ff81ee38fe4edbcd66
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2020-08-23 21:54:02 +0530

    append additional attribute so that the froneend would not have to compute so much

--- app/Site.php
+++ app/Site.php
@@ -6,9 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Site extends Model
 {
+    protected $appends = [
+        'css_color',
+        'status_word',
+    ];
+
     protected $casts = [
         'online' => 'boolean',
     ];
+
+    public function getCssColorAttribute() {
+        return $this->online ? 'success' : 'danger';
+    }
+
+    public function getStatusWordAttribute() {
+        return $this->online ? 'ONLINE' : 'OFFLINE';
+    }
 
     public function downtimes() {
         return $this->hasMany(Downtime::class);
