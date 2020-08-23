388d388dc60e443c2197aa865f7f0dc670081457
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2020-08-23 21:55:10 +0530

    show basic information about sites in a table

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,4 +2,27 @@
 
 @section('container')
 <h1>Web Ping</h1>
+
+@if(count($sites) > 0)
+    <table class="table">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Status</th>
+                <th>URL</th>
+                <th>Last Updated At</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($sites as $site)
+            <tr class="text-{{ $site->css_color }}">
+                <td>{{ $site->id }}</td>
+                <td>{{ $site->status_word }}</td>
+                <td><a href="{{ $site->url }}">{{ $site->url }}</a></td>
+                <td>{{ $site->updated_at }}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+@endif
 @endsection
