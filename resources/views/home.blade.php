40b7026ec214537f378c653a3163013402aa0606
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2020-08-23 22:46:13 +0530

    create form to add a new site

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,14 @@
 @section('container')
 <h1>Web Ping</h1>
 
+<div>
+    <form method="POST" action="/site" class="form-inline">
+        @csrf
+        <input type="text" name="url" class="form-control mb-2 mr-sm-2" placeholder="Enter URL here">
+        <button type="submit" class="btn btn-outline-primary mb-2">Add</button>
+    </form>
+</div>
+
 @if(count($sites) > 0)
     <table class="table">
         <thead>
