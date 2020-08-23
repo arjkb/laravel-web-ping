683f1ee136f3ffe66b712459862d6357980cfa12
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2020-08-23 21:24:38 +0530

    modify views to have a base layout template

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel Web Ping</title>
-</head>
-<body>
-    <h1>Web Ping</h1>
-    <div>
-        <p>Hello World</p>
-    </div>
-</body>
-</html>
+@extends('layouts.master')
+
+@section('container')
+<h1>Web Ping</h1>
+@endsection
